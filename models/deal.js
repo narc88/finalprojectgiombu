ac2fcbe1c7bfc6c2a8f99a4775fada8debd3a338
Nicolas Ronchi <nronchi@giumbu.com>
2012-07-12 09:03:11 -0300

    asdasda

---
+++ models/deal.js
@@ -0,0 +1,39 @@
+// Creación de la Conexión
+var mongoose        = require('mongoose')
+  , db_lnk          = 'mongodb://localhost/giombu'
+  , db              = mongoose.createConnection(db_lnk)
+
+
+var Schema = require('mongoose').Schema
+
+var deal_schema = new Schema({
+	title				: { type: String, required: true },
+	tagline				: { type: String, required: true },
+	slug				: { type: String, required: true },
+	characteristics		: { type: String, required: true },
+	conditions			: { type: String, required: true },
+	price				: { type: Number, required: true },
+	special_price		: { type: Number, required: true },
+	discount			: { type: Number, required: true, min:0, max:99 },
+	start_date        	:   Date,
+	end_date        	:   Date,
+	start_redeem        :   Date,
+	end_redeem        	:   Date,
+	max_coupons			: { type: Number, required: true, min:0},
+	max_coupons_by_user	: { type: Number, required: true, min:0},
+	seller_percentage	: { type: Number, required: true, min:0, max:99 },
+	giombu_percentage	: { type: Number, required: true, min:0, max:99 },
+	promoter_percentage	: { type: Number, required: true, min:0, max:99 },
+	status				: { type: String },
+	sale_count			: { type: Number, required: true, min:0},
+	coupon_count		: { type: Number, required: true, min:0},
+	shipping_cost		: { type: Number, required: true, min:0},
+	store   			: [{ type: Schema.ObjectId, ref: 'Store' }],
+	seller   			: [{ type: Schema.ObjectId, ref: 'Seller' }],
+	franchisor   		: [{ type: Schema.ObjectId, ref: 'Franchisor' }],
+	currency	   		: [{ type: Schema.ObjectId, ref: 'Currency' }],
+	created    		    :   Date,
+	modifiend        	:   Date
+})
+
+module.exports = deal_schema
---
+++ models/product.js
@@ -0,0 +1,34 @@
+// Creación de la Conexión
+var mongoose        = require('mongoose')
+  , db_lnk          = 'mongodb://localhost/giombu'
+  , db              = mongoose.createConnection(db_lnk)
+
+
+var Schema = require('mongoose').Schema
+
+var product_schema = new Schema({
+	code				: { type: String, required: true },
+	brand   			: [{ type: Schema.ObjectId, ref: 'Brand' }],
+	slug				: { type: String, required: true },
+	name				: { type: String, required: true },
+	short_name			: { type: String, required: true },
+	description			: { type: String, required: true },
+	price				: { type: Number, required: true },
+	special_price		: { type: Number, required: true },
+	discount			: { type: Number, required: true, min:0, max:99 },
+	stock				: { type: Number, required: true, min:0},
+	seller_percentage	: { type: Number, required: true, min:0, max:99 },
+	giombu_percentage	: { type: Number, required: true, min:0, max:99 },
+	promoter_percentage	: { type: Number, required: true, min:0, max:99 },
+	status				: { type: String },
+	sale_count			: { type: Number, required: true, min:0},
+	shipping_cost		: { type: Number, required: true, min:0},
+	store   			: [{ type: Schema.ObjectId, ref: 'Store' }],
+	seller   			: [{ type: Schema.ObjectId, ref: 'Seller' }],
+	franchisor   		: [{ type: Schema.ObjectId, ref: 'Franchisor' }],
+	currency	   		: [{ type: Schema.ObjectId, ref: 'Currency' }],
+	created    		    :   Date,
+	modifiend        	:   Date
+})
+
+module.exports =  product_schema
